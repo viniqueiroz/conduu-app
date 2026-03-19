49344ba9023fef3d084fd17fbd69342785ab4020
Vinicius Moura de Queiroz <vinicius.queiroz@codeba.gov.br>
2026-03-19 12:56:58 -0300

    feat: add gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true
+  /* config options here */
 };
 
 export default nextConfig;
